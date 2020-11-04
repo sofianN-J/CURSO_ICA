b601100e2024e45852188aa56fe3ded9384a50e6
DESKTOP-IOA93HQ\sofia <sofian-naimi@hotmail.fr>
2020-11-04 15:49:48 +0100

    components

---
+++ app-practica-vue/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+<!-- Componente num UNO -->
+<!-- Nombre componente -->
+<!-- <EtiquetaComp></EtiquetaComp> -->
+
+<!-- Componente num DOS -->
+<!-- Nombre componente -->
+<!-- <EtiquetaComp></EtiquetaComp> -->
+
+<!-- Componente tercero -->
+<!-- Nombre componente -->
+<!-- <EtiquetaComp></EtiquetaComp> -->
+
+<!--Asincronos-->
+<!-- AsincronosSofian -->
+<!-- <EtiquetaComp></EtiquetaComp> -->
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
